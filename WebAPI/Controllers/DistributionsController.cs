008c5f1fe983a549354ff4583e457beea7f1bebc
kianny_porras <kianny.valeriapg@gmail.com>
2026-07-20 21:15:26 -0600

    Estructura de controllers WebAPI

---
+++ WebAPI/Controllers/DistributionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class DistributionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
