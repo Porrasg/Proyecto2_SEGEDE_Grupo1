a65819ed89dbd5a658779f3feb498f5f82f5b977
kianny_porras <kianny.valeriapg@gmail.com>
2026-07-21 19:27:13 -0600

    Controllers

--- WebAPI/Controllers/FailuresController.cs
+++ WebAPI/Controllers/FailuresController.cs
@@ -1,12 +1,155 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreApp;
+using Entities_DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    public class FailuresController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FailuresController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
         {
-            return View();
+            try
+            {
+                var fm = new FailureManager();
+                var lstResults = fm.RetrieveAllFailures();
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveById/{id}")]
+        public ActionResult RetrieveById(int id)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                var failure = fm.RetrieveById(id);
+                return Ok(failure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveByTurbineId/{turbineId}")]
+        public ActionResult RetrieveByTurbineId(int turbineId)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                var lstResults = fm.RetrieveByTurbineId(turbineId);
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveByEngineerId/{engineerId}")]
+        public ActionResult RetrieveByEngineerId(int engineerId)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                var lstResults = fm.RetrieveByEngineerId(engineerId);
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveByStatus/{status}")]
+        public ActionResult RetrieveByStatus(string status)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                var lstResults = fm.RetrieveByStatus(status);
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveBySeverity/{severity}")]
+        public ActionResult RetrieveBySeverity(string severity)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                var lstResults = fm.RetrieveBySeverity(severity);
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult Create(Failure failure)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                fm.Create(failure);
+                return Ok(failure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult Update(Failure failure)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                fm.Update(failure);
+                return Ok(failure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(Failure failure)
+        {
+            try
+            {
+                var fm = new FailureManager();
+                fm.Delete(failure);
+                return Ok(failure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
--- WebAPI/Controllers/TurbinesController.cs
+++ WebAPI/Controllers/TurbinesController.cs
@@ -1,12 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreApp;
+using Entities_DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    public class TurbinesController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurbinesController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
         {
-            return View();
+            try
+            {
+                var tm = new TurbineManager();
+                var lstResults = tm.RetrieveAllTurbines();
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult Create(Turbine turbine)
+        {
+            try
+            {
+                var tm = new TurbineManager();
+                tm.Create(turbine);
+                return Ok(turbine);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult Update(Turbine turbine)
+        {
+            try
+            {
+                var tm = new TurbineManager();
+                tm.Update(turbine);
+                return Ok(turbine);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(Turbine turbine)
+        {
+            try
+            {
+                var tm = new TurbineManager();
+                tm.Delete(turbine);
+                return Ok(turbine);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
